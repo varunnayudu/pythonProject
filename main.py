6777c94af183b5dda4074f01415cc90252e95ed2
varunnayudu <varunnayudu@gmail.com>
2022-08-13 14:02:26 +0530

    unit test cases

--- main.py
+++ main.py
@@ -1,71 +1,71 @@
 import json
 
 
-
-
-
-
-def fetch_data_from_file(file_path):
-    file = open(file_path)
-    file_data = json.loads(file.read())
-    # print(file_data)
-    return file_data
-
-
-def bmi_calculator(weight, height):
-    _value = (weight / (height * height))
-    return _value
-
-
-def populate(_inputObj, _table_one):
-    bmi = round(bmi_calculator(_inputObj['WeightKg'], (_inputObj['HeightCm'] * 0.01)), 2)
-    #print("Bmi : ", bmi)
-    # iterate through table to find the category and other details
-    for table_one in _table_one:
-        if float(table_one['bmi_range'].split(" ")[0]) < bmi:
-            bmi_obj = table_one
-        else:
-            break
-    _inputObj.update(bmi_obj)
-
-
-def append_output_file(_inputObj, output_array):
-    output_array.append(_inputObj)
-    output_array_string = json.dumps(output_array)
-    with open("output.txt", "w") as output_file:
-        output_file.write(output_array_string)
-
-def countOverweight(output_array):
-    count=0
-    for output in output_array:
-        if output['bmi_category'] == 'Overweight' \
-                or output['bmi_category'] == 'Severely obese' \
-                or output['bmi_category'] == 'Very severely obese':
-            count=count+1
-
-    print('Overweight count :', count)
-
-
-
-
-
-
 class BMI:
+    def fetch_data_from_file(file_path):
+        file = open(file_path)
+        file_data = json.loads(file.read())
+        # print(file_data)
+        file.close()
+        return file_data
+
+
+    def bmi_calculator(weight, height):
+        _value = (weight / (height * height))
+        return _value
+
+
+    def populate(_inputObj, _table_one):
+        bmi = round(BMI.bmi_calculator(_inputObj['WeightKg'], (_inputObj['HeightCm'] * 0.01)), 2)
+        #print("Bmi : ", bmi)
+        # iterate through table to find the category and other details
+        for table_one in _table_one:
+            if float(table_one['bmi_range'].split(" ")[0]) < bmi:
+                bmi_obj = table_one
+            else:
+                break
+        _inputObj.update(bmi_obj)
+
+
+    def append_output_file(_inputObj, output_array):
+        output_array.append(_inputObj)
+        output_array_string = json.dumps(output_array)
+        with open("output.txt", "w") as output_file:
+            output_file.write(output_array_string)
+
+    def countOverweight(output_array):
+        count=0
+        for output in output_array:
+            if output['bmi_category'] == 'Overweight' \
+                    or output['bmi_category'] == 'Severely obese' \
+                    or output['bmi_category'] == 'Very severely obese':
+                count=count+1
+        return count
+
+
+
+
+
+
+
+
     def calculate(self):
-        _input_records = fetch_data_from_file("input.txt")
-        _table_one = fetch_data_from_file("table_one.txt")
+        _input_records = BMI.fetch_data_from_file("input.txt")
+        _table_one = BMI.fetch_data_from_file("table_one.txt")
         print('--start--')
         output_array=[]
         for _inputObj in _input_records:
             #print(_inputObj)
-            populate(_inputObj, _table_one)
+            BMI.populate(_inputObj, _table_one)
             #print(_inputObj)
-            append_output_file(_inputObj,output_array)
+            BMI.append_output_file(_inputObj,output_array)
 
         #print(output_array)
         print('..saving bmi calculate data into output.txt ')
-        countOverweight(output_array)
+        count = BMI.countOverweight(output_array)
+        print('Overweight count :', count)
         print('--complete--')
+
 
 bmi = BMI()
 bmi.calculate()
--- test_main.py
+++ test_main.py
@@ -2,18 +2,30 @@ import unittest
 import json
 from unittest import TestCase
 from unittest.mock import patch, mock_open
-from main import fetch_data_from_file
 
+from main import BMI
 
 
 class TestMain(unittest.TestCase):
     def test_fetch_data_from_file(self):
-        result = fetch_data_from_file("input.txt")
-        self.assertEqual(True, result.)  # add assertion here
+        result = BMI.fetch_data_from_file("input.txt")
+        self.assertEqual(6, len(result))
 
+    def test_bmi_calculator(self):
+        result = BMI.bmi_calculator(96, 1.71)
+        self.assertEqual(32.83, round(result, 2))
+
+    def test_count_overweight(self):
+        output_array= [
+            {"Gender": "Male", "HeightCm": 171, "WeightKg": 96, "bmi_category": "Moderately obese", "bmi_range": "30 - 34.9", "health_risk": "Medium risk"},
+            {"Gender": "Male", "HeightCm": 161, "WeightKg": 85, "bmi_category": "Moderately obese", "bmi_range": "30 - 34.9", "health_risk": "Medium risk"},
+            {"Gender": "Male", "HeightCm": 180, "WeightKg": 77, "bmi_category": "Normal weight", "bmi_range": "18.5 - 24.9", "health_risk": "Low risk"},
+            {"Gender": "Female", "HeightCm": 166, "WeightKg": 62, "bmi_category": "Normal weight", "bmi_range": "18.5 - 24.9", "health_risk": "Low risk"},
+            {"Gender": "Female", "HeightCm": 150, "WeightKg": 70, "bmi_category": "Moderately obese", "bmi_range": "30 - 34.9", "health_risk": "Medium risk"},
+            {"Gender": "Female", "HeightCm": 167, "WeightKg": 82, "bmi_category": "Overweight", "bmi_range": "25 - 29.9", "health_risk": "Enhanced risk"}
+        ]
+        result = BMI.countOverweight(output_array)
+        self.assertEqual(1, result)
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
